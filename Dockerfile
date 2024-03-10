07120eb56a05341763dc0760e90e63a28548d865
Ramesh Kumar M <rk.malempati@gmail.com>
2024-03-10 16:38:38 +0000

    Docker File created for new image -Ramesh M

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+  FROM openjdk:8
+  COPY . /var/www/java
+  WORKDIR /var/www/java
+  RUN javac RameshHelloWorld.java
+  CMD ["java", "Hello Ramesh"]
---
+++ RameshHelloWorld.java
@@ -0,0 +1,5 @@
+ class RameshHelloWorld {
+    public static void main(String[] args) {
+      System.out.println("This is a java app created by Ramesh using Docker");
+    }
+  }
